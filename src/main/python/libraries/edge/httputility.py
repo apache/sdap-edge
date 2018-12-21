9dd5732d0448742007d3344e3fd68d91030eb11e
Debellis <maya.debellis@jpl.nasa.gov>
2018-12-20 16:04:34 -0800

    setting default headers for passthrough

--- src/main/python/libraries/edge/httputility.py
+++ src/main/python/libraries/edge/httputility.py
@@ -2,12 +2,13 @@ import tornado.httpclient
 
 class HttpUtility(object):
     def getResponse(self, url, callback, body=None, headers=None):
-        requestHeaders = {'Connection': 'close'}
+        requestHeaders = {'Connection': 'close', 'User-Agent': 'Tornado', "Accept": "*"}
         if headers is not None:
             requestHeaders.update(headers)
         if body is not None:
             request = tornado.httpclient.HTTPRequest(url, method='POST', headers=requestHeaders, request_timeout=30, body=body)
         else:
             request = tornado.httpclient.HTTPRequest(url, method='GET', headers=requestHeaders, request_timeout=30)
+
         httpClient = tornado.httpclient.AsyncHTTPClient()
         httpClient.fetch(request,callback=callback)
