182a9b2cce78f423add350a51c5eaa3efe0ccdeb
Maya DeBellis <mayadebellis@gmail.com>
2019-03-25 10:30:57 -0700

    SDAP-169 Update passthrough plugin headers (#2)
    * setting default headers for passthrough
    
    * setting default headers for passthrough
    
    * add ucar

--- src/main/python/libraries/edge/httputility.py
+++ src/main/python/libraries/edge/httputility.py
@@ -2,7 +2,7 @@ import tornado.httpclient
 
 class HttpUtility(object):
     def getResponse(self, url, callback, body=None, headers=None):
-        requestHeaders = {'Connection': 'close'}
+        requestHeaders = {'Connection': 'close', 'User-Agent': 'Tornado', "Accept": "*"}
         if headers is not None:
             requestHeaders.update(headers)
         if body is not None:
